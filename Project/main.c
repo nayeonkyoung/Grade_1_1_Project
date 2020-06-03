656b32f74611a754001c1907a9ddba8605af8792
YeonKyoung <YeonKyoung@DESKTOP-MHD04BP>
2020-06-03 21:06:33 +0900

    초기 커밋.

---
+++ Project/main.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+	int a1;
+	printf("--Task 문서 중앙화 관리 프로그램—-\n\n");
+	printf("작업을 수행하려면 아래에 입력 해주세요!\n");
+	printf("[1. 사용자 정보 입력    2. 프로그램 종료]\n");
+	printf("번호 입력 : ");
+	scanf("%d",&a1);
+
+	switch (a1)
+	{
+	case 1 : 
+
+	default:
+		break;
+	}
+}
